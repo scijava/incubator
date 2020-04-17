bdf557bb3d46d466f894fec939873f68215d94f0
Gabriel Selzer <gjselzer@wisc.edu>
2020-04-17 10:07:05 -0500

    WIP: Open Type Extractors to scijava-common
    If we do not do this, then scijava-common cannot create an instance of
    them, which will prevent the types that they reify from being correctly
    reified

--- scijava/scijava-types/src/main/java/module-info.java
+++ scijava/scijava-types/src/main/java/module-info.java
@@ -1,7 +1,14 @@
-module org.scijava.types{
-  exports org.scijava.types;
-  
-  requires org.scijava;
-  // TODO: fix
+module org.scijava.types {
+
+	exports org.scijava.types;
+
+	// TODO: We need to open these classes to scijava-common so that the plugins
+	// can make use of the Type Extractors within.
+	// Decisions:
+	// Separate module for the type extractors?
+	opens org.scijava.types to org.scijava;
+
+	requires org.scijava;
+	// TODO: fix
 	requires com.google.common;
 }
