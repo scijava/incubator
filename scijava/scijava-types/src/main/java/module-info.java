44329edc62a3eb840dbd704dfdb66329438b89d2
Curtis Rueden <ctrueden@wisc.edu>
2020-04-20 15:38:02 -0500

    Force execution of SJC annotation processor

--- scijava/scijava-types/src/main/java/module-info.java
+++ scijava/scijava-types/src/main/java/module-info.java
@@ -2,11 +2,9 @@ module org.scijava.types {
 
 	exports org.scijava.types;
 
-	opens org.scijava.types.extractors to scijava.common;
-	// FIXME: This was put in so that scijava-common can access the TypeService.
-	// Ideally we would narrow its access.
-	opens org.scijava.types to scijava.common;
+	opens org.scijava.types.extractors to org.scijava;
+	opens org.scijava.types to org.scijava;
 
 	requires com.google.common;
-	requires scijava.common;
+	requires org.scijava;
 }
