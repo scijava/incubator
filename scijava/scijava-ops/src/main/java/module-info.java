c6137453bfe72bd7ad71c7ba97d484250a900417
Gabriel Selzer <gjselzer@wisc.edu>
2020-04-24 16:14:52 -0500

    WIP: Fix compile errors and redo commit

--- scijava/scijava-ops/src/main/java/module-info.java
+++ scijava/scijava-ops/src/main/java/module-info.java
@@ -1,7 +1,13 @@
 module org.scijava.ops {
 
   exports org.scijava.ops;
+  exports org.scijava.ops.function to org.scijava;
+  exports org.scijava.ops.adapt.functional to org.scijava;
+  exports org.scijava.ops.util to org.scijava;
   opens org.scijava.ops to org.scijava;
+  opens org.scijava.ops.function to org.scijava;
+  opens org.scijava.ops.adapt.functional to org.scijava;
+  opens org.scijava.ops.util to org.scijava;
 
   // FIXME: This is a file name and is thus unstable
   requires geantyref;
