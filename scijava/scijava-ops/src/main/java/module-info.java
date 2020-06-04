b4c5c7f33ccbf0182e163299e88d64838aaee5b5
Gabriel Selzer <gjselzer@wisc.edu>
2020-06-04 16:45:35 -0500

    WIP: Clean up ops module-info
    We do not need to export most of these packages, and apparently we do
    not need to open most of it to scijava-common as well

--- scijava/scijava-ops/src/main/java/module-info.java
+++ scijava/scijava-ops/src/main/java/module-info.java
@@ -1,42 +1,13 @@
 module org.scijava.ops {
+	
+	exports org.scijava.ops.function; // contains functional inferfaces
+	exports org.scijava.ops.core; // contains OpCollection, Op interfaces
+	exports org.scijava.ops.core.builder; // contains OpBuilder classes
+	// TODO: move OpWrapper to its own package (org.scijava.ops.wrap??)
+	exports org.scijava.ops.util; // contains OpWrapper interface
 
-  exports org.scijava.monitor;
-	exports org.scijava.ops;
-  exports org.scijava.ops.adapt;
-  exports org.scijava.ops.adapt.complexLift;
-  exports org.scijava.ops.adapt.functional;
-  exports org.scijava.ops.adapt.lift;
-  exports org.scijava.ops.copy;
-  exports org.scijava.ops.core;
-  exports org.scijava.ops.core.builder;
-  exports org.scijava.ops.create;
-  exports org.scijava.ops.function;
-  exports org.scijava.ops.log;
-	exports org.scijava.ops.matcher;
-	exports org.scijava.ops.math;
-	exports org.scijava.ops.stats;
-	exports org.scijava.ops.util;
-	exports org.scijava.param;
-	exports org.scijava.struct;
-
-  opens org.scijava.monitor to org.scijava;
+	// -- Open plugins to scijava-common
 	opens org.scijava.ops to org.scijava;
-  opens org.scijava.ops.adapt to org.scijava;
-  opens org.scijava.ops.adapt.complexLift to org.scijava;
-  opens org.scijava.ops.adapt.functional to org.scijava;
-  opens org.scijava.ops.adapt.lift to org.scijava;
-  opens org.scijava.ops.copy to org.scijava;
-  opens org.scijava.ops.core to org.scijava;
-  opens org.scijava.ops.core.builder to org.scijava;
-  opens org.scijava.ops.create to org.scijava;
-  opens org.scijava.ops.function to org.scijava;
-  opens org.scijava.ops.log to org.scijava;
-	opens org.scijava.ops.matcher to org.scijava;
-	opens org.scijava.ops.math to org.scijava;
-	opens org.scijava.ops.stats to org.scijava;
-	opens org.scijava.ops.util to org.scijava;
-	opens org.scijava.param to org.scijava;
-	opens org.scijava.struct to org.scijava;
 
   // FIXME: This is a file name and is thus unstable
   requires geantyref;
