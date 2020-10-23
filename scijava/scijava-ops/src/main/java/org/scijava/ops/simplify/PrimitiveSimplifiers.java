fa7092bcd477ba0872c7136c7cd42a107f91d847
Gabriel Selzer <gjselzer@wisc.edu>
2020-10-23 11:35:39 -0500

    Add simplifiers for all boxed primitive Numbers

--- scijava/scijava-ops/src/main/java/org/scijava/ops/simplify/PrimitiveSimplifiers.java
+++ scijava/scijava-ops/src/main/java/org/scijava/ops/simplify/PrimitiveSimplifiers.java
@@ -12,6 +12,44 @@ import org.scijava.plugin.Plugin;
 public class PrimitiveSimplifiers {
 
 	@Plugin(type = Simplifier.class)
+	public static class ByteSimplifier implements Simplifier<Number, Byte> {
+
+		@Override
+		public Number simplify(Byte p) {
+			return p;
+		}
+
+		@Override
+		public Byte focus(Number g) {
+			return g.byteValue();
+		}
+
+		@Override
+		public String toString() {
+			return "Byte Simplifier";
+		}
+	}
+
+	@Plugin(type = Simplifier.class)
+	public static class ShortSimplifier implements Simplifier<Number, Short> {
+
+		@Override
+		public Number simplify(Short p) {
+			return p;
+		}
+
+		@Override
+		public Short focus(Number g) {
+			return g.shortValue();
+		}
+
+		@Override
+		public String toString() {
+			return "Short Simplifier";
+		}
+	}
+
+	@Plugin(type = Simplifier.class)
 	public static class IntegerSimplifier implements Simplifier<Number, Integer> {
 
 		@Override
@@ -31,25 +69,6 @@ public class PrimitiveSimplifiers {
 	}
 
 	@Plugin(type = Simplifier.class)
-	public static class DoubleSimplifier implements Simplifier<Number, Double> {
-
-		@Override
-		public Number simplify(Double p) {
-			return p;
-		}
-
-		@Override
-		public Double focus(Number g) {
-			return g.doubleValue();
-		}
-
-		@Override
-		public String toString() {
-			return "Double Simplifier";
-		}
-	}
-
-	@Plugin(type = Simplifier.class)
 	public static class LongSimplifier implements Simplifier<Number, Long> {
 
 		@Override
@@ -65,6 +84,44 @@ public class PrimitiveSimplifiers {
 		@Override
 		public String toString() {
 			return "Long Simplifier";
+		}
+	}
+
+	@Plugin(type = Simplifier.class)
+	public static class FloatSimplifier implements Simplifier<Number, Float> {
+
+		@Override
+		public Number simplify(Float p) {
+			return p;
+		}
+
+		@Override
+		public Float focus(Number g) {
+			return g.floatValue();
+		}
+
+		@Override
+		public String toString() {
+			return "Float Simplifier";
+		}
+	}
+
+	@Plugin(type = Simplifier.class)
+	public static class DoubleSimplifier implements Simplifier<Number, Double> {
+
+		@Override
+		public Number simplify(Double p) {
+			return p;
+		}
+
+		@Override
+		public Double focus(Number g) {
+			return g.doubleValue();
+		}
+
+		@Override
+		public String toString() {
+			return "Double Simplifier";
 		}
 	}
 
