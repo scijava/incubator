d090c1cc63f633e88bb468e999640ea36538a9ac
Curtis Rueden <ctrueden@wisc.edu>
2021-05-20 19:58:35 -0500

    Install scijava-taglets before the reactor build
    It's a dependency of the maven-javadoc-plugin, which means it needs
    to be installed before generating javadocs for any of the modules.

---
+++ .travis/build.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+(cd scijava/scijava-taglets && mvn install)
+curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/master/travis-build.sh
+sh travis-build.sh $encrypted_844f7d2110b9_key $encrypted_844f7d2110b9_iv
