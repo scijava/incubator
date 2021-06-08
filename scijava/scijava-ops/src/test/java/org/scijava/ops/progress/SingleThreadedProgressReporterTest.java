de1f6da3e6712eac98f378659242a13f742741b1
Gabriel Selzer <gjselzer@wisc.edu>
2021-06-08 12:21:03 -0500

    Rewrite "Op" as Field
    Makes things cleaner

--- scijava/scijava-ops/src/test/java/org/scijava/ops/progress/SingleThreadedProgressReporterTest.java
+++ scijava/scijava-ops/src/test/java/org/scijava/ops/progress/SingleThreadedProgressReporterTest.java
@@ -10,18 +10,37 @@ import org.scijava.ops.progress.impl.SingleThreadedReporter;
 public class SingleThreadedProgressReporterTest extends
 	AbstractTestEnvironment
 {
+	public final Inplaces.Arity1<int[][][]> updatePerRow = (io) -> {
+		// get ProgressReporter -- TODO: Can we make this cleaner?
+		ProgressReporter p = ProgressReporters.getReporter(this.updatePerRow);
+		if (p == null || p.hasStarted()) throw new IllegalArgumentException();
+		p.reportStart();
+
+		// Process io
+		for (int i = 0; i < io.length; i++) {
+			for (int j = 0; j < io[i].length; j++) {
+				for (int k = 0; k < io[i][j].length; k++) {
+					io[i][j][k] = 1;
+				}
+				p.reportPixels(io[i][j].length);
+			}
+		}
+	};
 
 	@Test
-	public void testSingleThreaded() throws InterruptedException {
+	public void testSingleThreadedPerRowUpdate() throws InterruptedException {
+		TestInlace(updatePerRow);
+	}
+
+	private void TestInlace(Inplaces.Arity1<int[][][]> i) throws InterruptedException {
 		// setup object to be processed
 		int[][][] cube = new int[100][100][100];
 
 		// setup "Op" to run in another thread (so that we can track its progress
 		// here)
-		ThreadThing t = new ThreadThing();
-		ProgressReporter p = new SingleThreadedReporter(t, 100 * 100 * 100);
-		ProgressReporters.setReporter(t, p);
-		Thread thread = new Thread(() -> t.accept(cube));
+		ProgressReporter p = new SingleThreadedReporter(i, 100 * 100 * 100);
+		ProgressReporters.setReporter(i, p);
+		Thread thread = new Thread(() -> i.accept(cube));
 		thread.start();
 
 		// track progress
@@ -39,26 +58,3 @@ public class SingleThreadedProgressReporterTest extends
 
 }
 
-class ThreadThing implements Inplaces.Arity1<int[][][]> {
-
-	public ThreadThing() {}
-
-	@Override
-	public void mutate(int[][][] io) {
-		// get ProgressReporter
-		ProgressReporter p = ProgressReporters.getReporter(this);
-		if (p == null || p.hasStarted()) throw new IllegalArgumentException();
-		p.reportStart();
-
-		// Process io
-		for (int i = 0; i < io.length; i++) {
-			for (int j = 0; j < io[i].length; j++) {
-				for (int k = 0; k < io[i][j].length; k++) {
-					io[i][j][k] = 1;
-				}
-				p.reportPixels(io[i][j].length);
-			}
-		}
-	}
-
-}
