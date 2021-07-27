011f46497ae0febc8ad1155a331e869a8b342392
Gabriel Selzer <gjselzer@wisc.edu>
2021-07-27 11:19:26 -0500

    BIG WIP: Add scijava-common dependency
    Ideally, we wouldn't need SJC for this, but we need ValidityProblem

--- scijava/scijava-ops-api/src/main/java/module-info.java
+++ scijava/scijava-ops-api/src/main/java/module-info.java
@@ -2,8 +2,10 @@ module org.scijava.ops.api {
 
 	exports org.scijava.ops.api;
 
+	requires org.scijava;
 	requires org.scijava.function;
 	requires org.scijava.struct;
+	requires org.scijava.ops.spi;
 	requires org.scijava.types;
 
 }
